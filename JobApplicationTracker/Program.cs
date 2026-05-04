b516265b8d0e7737d26f1a6b85de4bbf1e04dd5c
Mohammed Yusur <mohammed_yusur@hotmail.com>
2026-05-04 11:56:04 +0200

    update

--- JobApplicationTracker/Program.cs
+++ JobApplicationTracker/Program.cs
@@ -3,29 +3,30 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Registrera databasen
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlite("Data Source=jobs.db"));
-
+// Registrera tjänster
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(connectionString));
 builder.Services.AddControllers();
-
-// Lägg till Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options => options.AddPolicy("AllowAll", policy =>
+{
+    policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+}));
 
 var app = builder.Build();
 
-// Skapa databasen vid behov
+// Skapa databasen om den inte finns
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
     db.Database.EnsureCreated();
 }
 
-// Aktivera Swagger
+// Middleware-konfiguration
+app.UseCors("AllowAll");
 app.UseSwagger();
-app.UseSwaggerUI();
-
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "JobTracker API V1"));
 app.MapControllers();
-
 app.Run();
+
